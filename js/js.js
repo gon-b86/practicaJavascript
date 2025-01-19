e5490cc493fd8c0014d9a7651a9e9037018c739c
Gonzalo <belmonte86@gmail.com>
2025-01-20 00:46:46 +0100

    Primer test de botones para Agregar al carrito hecho

--- js/js.js
+++ js/js.js
@@ -4,13 +4,13 @@
 const buttonsSigAnt = document.querySelectorAll('.container #flex button');
 const containerPlantas = document.querySelector('.container #plantas');
 
+
 // Función y Listener para manejar el evento de clic y cargar la página correspondiente
 
 function goToPage(event) {
-    const listado = parseInt(event.target.dataset.listado);  // obtiene el listado de productos a mostrar
-    
-    if (listado) {
-        switch (listado) {
+    const pagina = parseInt(event.target.dataset.object); 
+    if (pagina) {
+        switch (pagina) {
             case 1:
                 init(productos1);
                 break;
@@ -27,7 +27,7 @@ function goToPage(event) {
     }
 }
 
-buttonsSigAnt.forEach(button => button.addEventListener('click',goToPage))
+buttonsSigAnt.forEach(button => button.addEventListener('click',goToPage));
 
 // función para crear y pintar los artículos
 /*
@@ -73,11 +73,76 @@ function printAllProducts(list, dom){
 // función par a iniciar y en la que se definen los datasets de los botones de paginación
 
 function init (list){
-    buttonsSigAnt[0].dataset.listado = list.info.prev ? list.info.prev : null;
-    buttonsSigAnt[1].dataset.listado = list.info.next ? list.info.next : null;
+
+    buttonsSigAnt[0].dataset.object = list.info.prev ? list.info.prev : null;
+    buttonsSigAnt[1].dataset.object = list.info.next ? list.info.next : null;
+
     printAllProducts(list.results, containerPlantas);
+
     console.log(list.results)
+
+
+
+    // A mirar con Juan (como sacar de aquí esta función pero que funcione)
+    // A mirar con Juan (como sacar de aquí esta función pero que funcione)
+    // A mirar con Juan (como sacar de aquí esta función pero que funcione)
+    // A mirar con Juan (como sacar de aquí esta función pero que funcione)
+    // A mirar con Juan (como sacar de aquí esta función pero que funcione)
+    // A mirar con Juan (como sacar de aquí esta función pero que funcione)
+    // A mirar con Juan (como sacar de aquí esta función pero que funcione)
+    // A mirar con Juan (como sacar de aquí esta función pero que funcione)
+
+
+    function agregarCarrito(event){
+        /* <li>
+            <span>Planta - €Imp x Cant</span>
+            <button class="butEliminar">Eliminar</button>
+            <button class="butSignoMas">+</button>
+            <button class="butSignoMenos">-</button>
+        </li>*/
+        
+        const li = document.createElement('li');
+        const span = document.createElement('span')
+        const butEliminar = document.createElement('button')
+        const butSignoMas = document.createElement('button')
+        const butSignoMenos = document.createElement('button')
+        const hr = document.createElement('hr')
+        
+        span.textContent='Planta - €Imp x Cant';
+        butEliminar.textContent='Eliminar';
+        butSignoMas.textContent='+';
+        butSignoMenos.textContent='-';
+        butSignoMas.classList.add('mas');
+        butSignoMenos.classList.add('menos');
+        
+        li.append(span, butEliminar, butSignoMas, butSignoMenos);
+        carritoListado.append(li, hr);
+        
+    }
+
+    // capturo en el DOM el botón agregar al carrito y la caja donde pintarlo
+    const buttonAgregar = document.querySelectorAll('.container #plantas button');
+    const carritoListado = document.querySelector('#carrito #cajaListado #listado');
+    
+    // Función y listener para agregar al carrito
+    buttonAgregar.forEach(button => button.addEventListener('click',agregarCarrito));
+
+
+
+
 };
 
 // el script arranca cargando el primero objeto
 init (productos1)
+
+
+
+
+
+
+
+
+
+
+
+
